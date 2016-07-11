d0ecc65b0ef867e5cc6680fa4fc160d6cc8cea76
Cary M. Robbins <carymrobbins@gmail.com>
2016-07-11 17:12:48 -0500

    Create Builder monad

--- frege-lib/src/com/haskforce/frege/lang/parser/FregePsiParser.java
+++ frege-lib/src/com/haskforce/frege/lang/parser/FregePsiParser.java
@@ -5,7 +5,6 @@ import com.intellij.lang.PsiBuilder;
 import com.intellij.lang.PsiParser;
 import com.intellij.psi.tree.IElementType;
 import frege.prelude.PreludeBase;
-import frege.run8.Thunk;
 import org.jetbrains.annotations.NotNull;
 
 public class FregePsiParser implements PsiParser {
@@ -13,7 +12,7 @@ public class FregePsiParser implements PsiParser {
   @Override
   public ASTNode parse(@NotNull IElementType root, @NotNull PsiBuilder builder) {
     return PreludeBase.TST.<ASTNode>performUnsafe(
-      FregeParser.psiParse(Thunk.<IElementType>lazy(root), builder)
+      FregeParser.psiParse(root, builder)
     ).call();
   }
 }
